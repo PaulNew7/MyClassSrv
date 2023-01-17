7909dbf7fd29169582dfee444b8b6a99246a45c1
Polo <you@example.com>
2023-01-17 15:12:58 +0800

    添加models,依赖文件

---
+++ ReadMe.md
@@ -0,0 +1,28 @@
+# 环境搭建
+- python 国内镜像https://mirrors.huaweicloud.com/python/3.10.0/
+- pip3 install numpy -i https://pypi.tuna.tsinghua.edu.cn/simple
+- redis官方不支持windows,改用Memcached https://redis.io/docs/getting-started/installation/install-redis-on-windows/
+"D:\mydata\full_data\memcached-1.4.5-amd64\memcached-amd64\memcached.exe"
+- mysql安装很重，先用内存数据库
+- django C:\Users\Thomas\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages
+
+C:\Users\Thomas\AppData\Local\Programs\Python\Python310
+- node.js
+- - nmp
+# 插件安裝
+# 后端代码编写
+## 公共库缓存
+# 前端代码编写
+# 启动脚本
+# 自测用例
+# 运行
+## 修改DB和缓存配置
+## 初始化DB
+> python manage.py makemigrations
+
+> python manage.py migrate
+## 初始化数据
+> python manage.py createsuperuser
+> 
+ 注意!，windows环境需要在CMD中执行，否则报错
+> Superuser creation skipped due to not running in a TTY. You can run `manage.py createsuperuser` in your project to create one manually
--- UserControl/models.py
+++ UserControl/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+from django.core.validators import RegexValidator
+
+# alpha_spaces = RegexValidator(r'^[a-zA-Z]*$', 'Only letters and spaces allowed.')
+num_only = RegexValidator(r'^[0-9 ]*$', 'Only numbers allowed.')
+
+
+class Student(models.Model):
+    gender_choices = (
+        (1, '男'), (2, '女'), (3, '其他')
+    )
+    name = models.CharField(max_length=32, blank=False, null=False, db_index=True)
+    code = models.CharField(max_length=8, blank=False, null=False, validators=[num_only], unique=True)
+    gender = models.SmallIntegerField(blank=False, null=False, choices=gender_choices)
+    my_class = models.ForeignKey('Class', on_delete=models.CASCADE, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
